b42001796c3cbaf7b529612cf1735e846ea40cae
Stefan Milojević <stefanmilojevic45@gmail.com>
2021-04-08 13:54:50 +0200

    Added webconfig

---
+++ src/main/java/com/stefanmilojevic/myRealEstate/config/WebConfig.java
@@ -0,0 +1,57 @@
+package com.stefanmilojevic.myRealEstate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(passwordEncoder()).usersByUsernameQuery("select email, password"
+                + " from user where email=?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("register").permitAll()
+//                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                .antMatchers("/**").permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+
+}
--- src/main/java/com/stefanmilojevic/myRealEstate/repository/ProfileCommentsRepository.java
+++ src/main/java/com/stefanmilojevic/myRealEstate/repository/ProfileCommentsRepository.java
@@ -1,8 +1,9 @@
 package com.stefanmilojevic.myRealEstate.repository;
 
+import com.stefanmilojevic.myRealEstate.model.ProfileComments;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfileCommentsRepository extends JpaRepository<ProfileCommentsRepository, Long> {
+public interface ProfileCommentsRepository extends JpaRepository<ProfileComments, Long> {
 }
